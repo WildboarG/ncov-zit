8c0c8d342bdc8a4ffadcdf13c7964d22b976a483
WildboarG <959586@outlook.com>
2022-08-02 20:36:33 +0800

    ...

--- main.py
+++ main.py
@@ -3,38 +3,37 @@ Author: WildboarG
 version: 1.0
 Date: 2022-05-27 15:17:19
 LastEditors: WildboarG
-LastEditTime: 2022-05-28 13:37:19
+LastEditTime: 2022-08-02 20:21:54
 Descripttion: 
 '''
-import sys
-import json
-import os
 import csv
-import getopt
 import queue
 import threading
 
 from rich import print
 from rich.table import Table
 
-from info.login import GetMe
+from info.login import GetContent
 from info.post import sign
 
 class User:
     def __init__(self,schoolcode:str) -> None:
         self.schoolcode = schoolcode
         self.q = queue.Queue()
+        
+    def _get_data(self,information:GetContent):
+        return information.reporter()
 
     def __signed(self,username,password):
         info = []
         info.append(username)
-        getme = GetMe(
+        sign_content = GetContent(
             schoolcode = self.schoolcode,
-            usernamme = username,
-            password = password
+            username = username,
+            password = password,
         )
 
-        cook,data = getme.get_lastdata()
+        cook,data = self._get_data(sign_content)
         ## return status
         result = sign(data,cook)
         info.append(result)
@@ -52,18 +51,20 @@ class User:
                 writter = csv.writer(f)
                 writter.writerows([[self.username,self.password,self.schoolcode]])
                 print("[green]添加成功！[green]")        
-    def csv_sign(self):
-        ## 创建队列，返回队列对象
-        with open("./config/user.csv","r") as f:
-            reader = csv.reader(f)
-            readlist = list(reader)
+    
+    def _output_table(self,information :list)->Table:
+        table = Table(show_header=True,header_style="bold magenta")
+        for i in information:
+            table.add_column(i,justify="center")
+        return table
+    
+    def _print_table(self,table:Table)->Table:
+        print(table)
+
+    def _crate_Threads(self,rows:list):
         threads=[]
-        res = []
-        for row in readlist:
+        for row in rows:
             try:
-                # res = self.__signed(row[0],
-                #             row[1]
-                #     )
                 threads.append(
                         threading.Thread(
                             target=self.__signed,
@@ -78,22 +79,34 @@ class User:
         
         for t in threads:
             t.join()
+    
+    def csv_sign(self):
+        ## 创建队列，返回队列对象
+        with open("./config/user.csv","r") as f:
+            reader = csv.reader(f)
+            readlist = list(reader)
+        
+        res = []        
+        self._crate_Threads(readlist)
 
         while not self.q.empty():
             res.append(self.q.get())
         
-        table = Table(show_header=True,header_style="bold magenta")
-        table.add_column("Date",style="dim",width=12)
-        table.add_column("Id",justify="center")
-        table.add_column("Status",justify="center")
+
+        table = self._output_table(["ID","Status","Node"])
         for i in res:
-            print(i)
-            table.add_row(" Apr 28 2022",i[0],i[1])
-        
-        print(table)
+            table.add_row(i[0],i[1],"123")
+
+        self._print_table(table)
     ## 创建一个个人测试的函数
-    def test(self):
-        pass
+    def test(self,username,password)->str:
+        bin = GetContent(
+            schoolcode = self.schoolcode,
+            username = username,
+            password = password,
+        )
+        cook,data = bin.reporter()
+        print(sign(data,cook))
 if __name__ == "__main__":
     #user = User()
     user = User("zit")
