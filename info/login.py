e863b740abc01c4c9f66a5f874222261c2ecf8d8
WildboarG <959586@outlook.com>
2022-08-02 20:36:14 +0800

    优化代码

--- info/login.py
+++ info/login.py
@@ -3,15 +3,16 @@ Author: WildboarG
 version: 1.0
 Date: 2022-05-27 15:28:40
 LastEditors: WildboarG
-LastEditTime: 2022-05-28 11:49:56
+LastEditTime: 2022-08-02 20:32:16
 Descripttion: 
 '''
 from info import *
 import requests
 import json
-class GetMe:
-    def __init__(self, schoolcode : str ,usernamme : str,password : str):
-        self.username = usernamme
+class GetContent:
+
+    def __init__(self, schoolcode : str ,username : str,password : str):
+        self.username = username
         self.password = password
         self.schoolcode = schoolcode
         self.authorityid = authorityid
@@ -37,89 +38,72 @@ class GetMe:
                 "scope":"base_api",
                 "state":"ruijie"
                 }
+        self.params1 = {
+            "sch_code":self.schoolcode,
+            "stu_code": self.username,
+            "authorityid": json.dumps(self.authorityid),
+            "type": '1',
+            "pagenum": '1',
+            "pagesize": '20',
+            "stu_range": '999',
+            "searchkey": ""
+        }
 
-    def login(self):
+    def _get_cookie(self,realcook:str)->str:
+        return self.s.get(
+                url = str(realcook),
+                headers = self.headers,
+                allow_redirects = False
+            ).headers["set-cookie"]
 
-        get_cook = self.s.get(endpoint["login"])
-        cook =get_cook.headers.get("Set-Cookie")
-
-        self.headers["Cookie"] = cook
-        try:
-            self.s.post(
-                url=endpoint["login"],
-                headers=self.headers,
-                data=self.data,
-                allow_redirects=False
-            )
-            spaserver = self.s.get(
+    def _get_realcook(self) -> str:
+        spaserver = self.s.get(
                 url=endpoint["authorize"],
                 headers=self.headers,
                 params=self.params,
                 allow_redirects=False
             )
-            realcook = spaserver.text[22:]
-            cookie =self.s.get(
-                url = str(realcook),
-                headers = self.headers,
-                allow_redirects = False
-            ).headers["set-cookie"]
+        return spaserver.text[22:]
+
+    def _get_addr(self):
+        self.s.post(
+                url=endpoint["login"],
+                headers=self.headers,
+                data=self.data,
+                allow_redirects=False
+            )
+
+    def _get_init_cook(self)->str:
+        return self.s.get(endpoint["login"]).headers.get("Set-Cookie")
+    
+    def _login(self) -> str:
+        cook = self._get_init_cook()
+        self.headers["Cookie"] = cook
+        try:
+            self._get_addr()            
+            realcook = self._get_realcook()
+            cookie =self._get_cookie(realcook)
+
             return cookie
         
         except:
+            return None
 
-            return None
-    def get_lastdata(self):
-        cookie = self.login()
-        if not cookie:
-            return None
-        else:
-            head = {
-                #'Content-Type': 'application/json',
-                'Cookie': cookie
-            }
-            self.params1 = {
-                "sch_code":self.schoolcode,
-                "stu_code": self.username,
-                "authorityid": json.dumps(self.authorityid),
-                "type": '1',
-                "pagenum": '1',
-                "pagesize": '20',
-                "stu_range": '999',
-                "searchkey": ""
-            }
-        try:
-            ## 获取上次的info
-            data = self.s.get(
+    def _get_yesterday_info(self,header :str,params2 :str) -> str:
+        return self.s.get(
+                url = api["getQuestionDetail"],
+                headers = header,
+                params = params2,
+            ).json().get("data")
+
+    def _get_yesterday_data(self,header)->str:
+        return self.s.get(
                 url = api["getQuestionNaireList"],
-                headers = head,
+                headers = header,
                 params = self.params1,
             ).json().get("data")[0]
 
-            activityid = data.get("activityid")
-            authorityid = data.get("authorityid")
-
-        except:
-            return None
-        
-        try:
-            ## 获取问卷的详细信息
-            self.params2 = {
-                "sch_code": self.schoolcode,
-                "stu_code": self.username,
-                "activityid":json.dumps(activityid),
-                "can_repeat":"1",
-                "page_from":"onpublic",
-                "private_id":"0"
-            }
-            info = self.s.get(
-                url = api["getQuestionDetail"],
-                headers = head,
-                params = self.params2,
-
-            ).json().get("data")
-        except:
-            return None
-
+    def _handle_info(self,info,head,activityid)->str:
         try:
             questions = info.get("question_list")
             private_id = info.get("last_private_id")
@@ -202,7 +186,7 @@ class GetMe:
                     url = api["userInfo"],
                     headers=head
                 ).json().get("data")
-            data = {
+            return  {
                 "sch_code": userinfo.get("schcode"),
                 "stu_code": userinfo.get("stucode"),
                 "stu_name": userinfo.get("username"),
@@ -218,8 +202,43 @@ class GetMe:
                 "totalArr": totalArr,
                 "private_id": private_id
             }
-            return cookie,data
-        ## 没有解析出问卷的信息
         except:
             return None
+
+    def _get_params2(self,actionid:str)->str:
+            return {
+                "sch_code": self.schoolcode,
+                "stu_code": self.username,
+                "activityid":json.dumps(actionid),
+                "can_repeat":"1",
+                "page_from":"onpublic",
+                "private_id":"0"
+            }
+    def _get_lastdata(self,cookie :str)->str:
+        if not cookie:
+            return None
+        head = {
+            #'Content-Type': 'application/json',
+            'Cookie': cookie
+        }
+        try:
+            ## 获取上次的info
+            data = self._get_yesterday_data(head)
+            activityid = data.get("activityid")
+            params2 = self._get_params2(activityid)
+        except:
+            return None
+        
+        try:
+            ## 获取问卷的详细信息
+            info = self._get_yesterday_info(head,params2)
+            return self._handle_info(info,head,activityid)
+        except:
+            return None
+    # 
+    def reporter(self):
+        cookie = self._login()
+        data = self._get_lastdata(cookie)
+        return cookie,data
+        
 
